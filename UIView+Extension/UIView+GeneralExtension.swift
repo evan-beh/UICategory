0cfc3d890250c1e63a1e4143f3aa85b1bcf8ba62
Evan <evan.beh@razer.com>
2019-09-09 10:59:23 +0800

    first commit

---
+++ UIView+Extension/UIView+GeneralExtension.swift
@@ -0,0 +1,35 @@
+//
+//  UIView+GeneralExtension.swift
+//  TenthPlan
+//
+//  Created by evan beh on 9/9/19.
+//  Copyright © 2019 evan beh. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+    func roundBorders(value:Float)
+    {
+        self.layer.cornerRadius = CGFloat(value)
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.clear.cgColor
+    }
+    
+    func roundBorders(value:Float, borderColor:UIColor)
+    {
+        self.layer.cornerRadius = CGFloat(value)
+        self.layer.borderWidth = 1
+        self.layer.borderColor = borderColor.cgColor
+    }
+    
+    public func roundCorners(corners:UIRectCorner, radius: CGFloat)
+    {
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius)).cgPath
+        self.layer.mask = maskLayer
+        self.layer.masksToBounds = true
+    }
+    
+}
